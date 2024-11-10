752c047af1735f2687055cfa5889463f1d0f728c
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-11-10 17:15:14 +0545

    modified next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,  // Enable App Router
+  },
   images:{
     domains:[
         "res.cloudinary.com",
