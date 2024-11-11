e3178a22db82a28b1088da65097e1c33b448a737
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-11-11 15:51:02 +0545

    next-auth 4

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,22 @@
-// import { withAuth } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
 
-// export default withAuth({
-//   // Matches the pages config in `[...nextauth]`
-//   pages: {
-//     signIn: "/signin",
-//   },
-// })
+export default withAuth({
+  // Matches the pages config in `[...nextauth]`
+  pages: {
+    signIn: "/signin",
+  },
+})
 
-// export const config = {
-//   matcher: [
-//     "/experts/:path*",
-//     "/crops/:path*",
-//     "/diseases/:path*",
-//     "/chats/:path*",
-//     "/admin/:path*",
-//     "/expert/:path*",
-//     "/field/:path*",
-//     "/consult/:path*",
-//     "/predict/:path*",
-//     "/api/:path*"
-//   ],}
-
-export { auth as middleware } from "@/auth"
+export const config = {
+  matcher: [
+    "/experts/:path*",
+    "/crops/:path*",
+    "/diseases/:path*",
+    "/chats/:path*",
+    "/admin/:path*",
+    "/expert/:path*",
+    "/field/:path*",
+    "/consult/:path*",
+    "/predict/:path*",
+    "/api/:path*"
+  ],}
