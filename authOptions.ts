261e25e1a0acad60969916f788a7331c0470ead7
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-11-11 12:30:06 +0545

    new file:   app/api/auth/[...nextauth]/route.ts

--- authOptions.ts
+++ authOptions.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import NextAuth, { AuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
@@ -70,7 +70,3 @@ export const authOptions: AuthOptions = {
   },
   debug: process.env.NODE_ENV === "development",
 };
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
