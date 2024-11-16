3e9022763bcd91aba71151662ac9137cc72eed53
Bhim Prasad Adhikari <bhimprasadadhikari98@gmail.com>
2024-11-16 12:13:21 +0545

    update app.py

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -57,4 +57,4 @@ CORS(app)  # Allow all domains, or specify the domain of your Next.js app
 
 # Run the Flask app
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)  # Make sure Flask runs on port 5000
+    app.run()  # Make sure Flask runs on port 5000
